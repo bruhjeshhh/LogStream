49a8a2d023aafc94e7b2b306731aba5c6037b20a
Brajesh Mohanty <mohantybrajesh4@gmail.com>
2026-07-04 16:00:20 +0530

    feat(kafka): using reader and writer

---
+++ consumer/consumer.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	_ "time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+func main() {
+	// to consume messages
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:  []string{"localhost:9092"},
+		Topic:    "test-logs",
+		GroupID:  "test-consumer-group",
+		MaxBytes: 10e6, // 10MB
+	})
+
+	for {
+		m, err := r.ReadMessage(context.Background())
+		if err != nil {
+			break
+		}
+		fmt.Printf("message at offset %d: %s = %s\n", m.Offset, string(m.Key), string(m.Value))
+	}
+
+	if err := r.Close(); err != nil {
+		log.Fatal("failed to close reader:", err)
+	}
+}
