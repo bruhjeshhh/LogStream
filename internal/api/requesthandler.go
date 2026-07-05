131755a14d9623bffe30a6c904ba2f80bd6c92ee
Brajesh Mohanty <mohantybrajesh4@gmail.com>
2026-07-05 17:17:40 +0530

    feat(api): add request handling and validation of incoming requsts

---
+++ internal/api/requesthandler.go
@@ -0,0 +1,39 @@
+package api
+
+import (
+	"LogStream/internal/models"
+	"encoding/json"
+	"net/http"
+	"slices"
+	"strings"
+)
+
+func Ingest(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var payload []models.Ingestion
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&payload); err != nil {
+		http.Error(w, "Invalid JSON payload", http.StatusBadRequest)
+		return
+	}
+
+	var allowedLevels = []string{"trace", "debug", "info", "warn", "error", "fatal"}
+
+	validPayloads := payload[:0]
+
+	for _, pld := range payload {
+		if pld.Level != "" || pld.Message != "" || pld.Service != "" || isJSONObject(pld.Metadata) && slices.Contains(allowedLevels, strings.ToLower(pld.Level)) {
+			validPayloads = append(validPayloads, pld)
+		}
+	}
+}
+
+func isJSONObject(data []byte) bool {
+	var m map[string]any
+	// json.Unmarshal returns nil error only if it fits the target type
+	return json.Unmarshal(data, &m) == nil
+}
