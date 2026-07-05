e95546ab1c790b449847dfde7d8eb2d460f500bb
Brajesh Mohanty <mohantybrajesh4@gmail.com>
2026-07-06 00:17:38 +0530

    feat(service): add conversion of validated requests into logs

--- internal/service/ingest.go
+++ internal/service/ingest.go
@@ -2,8 +2,28 @@ package service
 
 import (
 	"LogStream/internal/models"
+	"log"
+	"time"
+
+	"github.com/google/uuid"
 )
 
-func Ingest([]models.Ingestion) {
+func Ingest(payload []models.Ingestion) {
+	var validPayloads []models.Log
 
+	for _, pld := range payload {
+		var vldpld models.Log
+		id, err := uuid.NewV7()
+		if err != nil {
+			log.Fatalf("Failed to generate UUIDv7: %v", err)
+		}
+		vldpld.ID = id
+		vldpld.Level = pld.Level
+		vldpld.Service = pld.Service
+		vldpld.EventTimestamp = pld.Timestamp
+		vldpld.ReceivedTimestamp = time.Now()
+		vldpld.Message = pld.Message
+		vldpld.Metadata = pld.Metadata
+		validPayloads = append(validPayloads, vldpld)
+	}
 }
