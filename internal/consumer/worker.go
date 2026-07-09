64766f7f50aab3bd2abe8756691ccb7dee0f7fdd
Brajesh Mohanty <mohantybrajesh4@gmail.com>
2026-07-10 00:31:02 +0530

    feat(consumer): add workers

---
+++ internal/consumer/worker.go
@@ -0,0 +1,33 @@
+package consumer
+
+import (
+	"LogStream/internal/models"
+	"context"
+	"encoding/json"
+	"log"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type Worker struct{}
+
+func NewWorker() *Worker {
+	return &Worker{}
+}
+
+func (w *Worker) Process(ctx context.Context, msg kafka.Message) error {
+	var entry models.Log
+	if err := json.Unmarshal(msg.Value, &entry); err != nil {
+		return err
+	}
+
+	log.Printf(
+		"processed log id=%s service=%s level=%s message=%q",
+		entry.ID,
+		entry.Service,
+		entry.Level,
+		entry.Message,
+	)
+
+	return nil
+}
